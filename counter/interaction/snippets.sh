735d97900b7508fb04e2e10e724481309ffcfa20
TTNguyenDev <nttriet9a4@gmail.com>
2022-10-17 23:11:40 +0700

    update interaction

--- counter/interaction/snippets.sh
+++ counter/interaction/snippets.sh
@@ -1,36 +1,28 @@
-PEM_FILE="./counter.pem"
+PEM_FILE="../../../wallet/wallet-owner.pem"
 COUNTER_CONTRACT="output/counter.wasm"
 
 PROXY_ARGUMENT="--proxy=https://devnet-api.elrond.com"
 CHAIN_ARGUMENT="--chain=D"
+
 
 build_counter() {
     (set -x; erdpy --verbose contract build "$COUNTER_CONTRACT")
 }
 
 deploy_counter() {
-    # local TOKEN_ID=0x45474c44 # "EGLD"
-    # local PING_AMOUNT=1500000000000000000 # 1.5 EGLD
-    # local DURATION=86400 # 1 day in seconds
-    # # local ACTIVATION_TIMESTAMP= # skipped
-    # # local MAX_FUNDS= #skipped
-    # 
-    # local OUTFILE="out.json"
-    # (set -x; erdpy contract deploy --bytecode="$COUNTER_CONTRACT" \
-    #     --pem="$PEM_FILE" \
-    #     $PROXY_ARGUMENT $CHAIN_ARGUMENT \
-    #     --outfile="$OUTFILE" --recall-nonce --gas-limit=60000000 \
-    #     --arguments ${PING_AMOUNT} ${DURATION} --send \
-    #     || return)
-    #
-    # local RESULT_ADDRESS=$(erdpy data parse --file="$OUTFILE" --expression="data['emitted_tx']['address']")
-    # local RESULT_TRANSACTION=$(erdpy data parse --file="$OUTFILE" --expression="data['emitted_tx']['hash']")
-    #
-    # echo ""
-    # echo "Deployed contract with:"
-    # echo "  \$RESULT_ADDRESS == ${RESULT_ADDRESS}"
-    # echo "  \$RESULT_TRANSACTION == ${RESULT_TRANSACTION}"
-    # echo ""
+    erdpy --verbose contract deploy --recall-nonce --project="../counter/" \
+          --metadata-payable \
+          --pem=${PEM_FILE} \
+          --gas-limit=150000000 \
+          --proxy=${PROXY} --chain=${CHAIN_ID} \
+          --outfile="deploy-testnet.interaction.json" --send --wait-result
+    
+    ADDRESS=$(erdpy data parse --file="deploy-testnet.interaction.json" --expression="data['emitted_tx']['address']")
+
+    erdpy data store --key=faucet-address --value=${ADDRESS}
+
+    echo ""
+    echo "Faucet Smart contract address: ${ADDRESS}"
 }
 
 number_to_u64() {
