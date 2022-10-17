21c7c6d96f3a9a3e5ecfdd25a1c03edf13d89193
TTNguyenDev <nttriet9a4@gmail.com>
2022-10-17 17:17:58 +0700

    init project

---
+++ counter/src/counter.rs
@@ -0,0 +1,41 @@
+#![no_std]
+
+elrond_wasm::imports!();
+
+#[elrond_wasm::contract]
+pub trait Counter {
+    #[init]
+    fn init(&self) {
+        self.count().set(BigInt::zero());
+    }
+
+    // endpoints
+    #[endpoint]
+    fn increment(&self) {
+        let count_val = self.count().get();
+        self.count().set(count_val + BigInt::from(1));
+    }
+
+    #[endpoint]
+    fn decrement(&self) {
+        let count_val = self.count().get();
+        self.count().set(count_val - BigInt::from(1));
+    }
+
+    #[endpoint]
+    fn reset(&self) {
+        self.count().set(BigInt::zero());
+    }
+
+    // views
+    #[view(getValue)]
+    fn get_num(self) -> BigInt {
+        self.count().get()
+    }
+
+    // storage
+
+    #[view(getNum)]
+    #[storage_mapper("count")]
+    fn count(&self) -> SingleValueMapper<BigInt>;
+}
---
+++ counter/wasm/src/lib.rs
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    counter
+    (
+        decrement
+        getNum
+        getValue
+        increment
+        reset
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
